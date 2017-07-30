f721b402acb23384079ea3cf068bd7c95898c381
Paul Scott <paul.scott@ravelin.com>
2017-07-30 22:30:48 +0100

    More docs

--- doc.go
+++ doc.go
@@ -1,3 +1,16 @@
 // Package shufn implements a pseudo-random iterator over a numeric range with
-// no memory overhead.
+// practically no memory overhead. shufn works on the principal that, given a
+// prime number p, a primitive root r of p, then i' = (i * r) % p will loop
+// through all integers [1, p) in a seemingly random order. This sequence is
+// consistent upon invocations. The start parameter can be randomized to shift
+// the sequence.
+//
+// shufn.Call provides a user-friendly interface for calculating an appropriate
+// prime p and primitive root r for a given range [min, max]. shufn.New can
+// then be used to instantiate the iterator immediately. Where the range is
+// known at compilation time, the results of shufn.Call can be hard-coded.
+//
+// The shufn executable (go get github.com/icio/shufn/cmd/shufn) provides this
+// precomputation on the command-line. For example: `shufn -q 1000000` will
+// demonstrate multiple ways to use the shufn package.
 package shufn
